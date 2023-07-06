155efaffce43aaafcb865f33f3aed3abde7ab1cf
wassim <wassim.hassiin@gmail.com>
2023-07-06 16:42:47 +0200

    commit

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,7 +3,6 @@ import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import "./login.css";
-import Registre from "../register/Registre"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faEnvelope , faMobile} from '@fortawesome/free-solid-svg-icons'
 import { ToastContainer, toast } from "react-toastify";
@@ -71,7 +70,7 @@ const Login = () => {
           Login
         </button>
         <div className="rrtext">
-         <Link to={<Registre/>}>create account</Link>
+         <Link to={'/registre'}>create account</Link>
         </div>
         {error && <span>{error.message}</span>}
         <p  className="logindesc">ou choisissez l'une de ces options</p>
