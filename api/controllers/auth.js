5de92b29599cf82d4324bb75bb14ad3f31997bfe
wassim <wassim.hassiin@gmail.com>
2023-07-08 01:54:01 +0200

    commit

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -36,9 +36,9 @@ export const login = async (req, res, next) => {
 
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
-      process.env.JWT ,
+      process.env.JWT 
+      // {expiresIn : '24h'}
     );
-    // expiresIn : '10s'
     const { password, isAdmin, ...otherDetails } = user._doc;
     res
       .cookie("access_token", token, {
